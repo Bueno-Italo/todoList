6f08066b3c7983c4f43463e293f5f442683066d8
Bueno-Italo <italo-bueno@hotmail.com>
2025-01-03 13:07:46 -0300

    Variaveis JS

---
+++ script.js
@@ -0,0 +1,6 @@
+const texto = document.querySelector('input')
+const btnInsert = document.querySelector('.divInsert button')
+const btnDeleteAll = document.querySelector('.header button')
+const ul = document.querySelector('ul')
+
+var itensDB = []
